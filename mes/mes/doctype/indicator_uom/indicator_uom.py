1f861a21e0e56f2072dfd1ba09bc90cae6a2638e
frappe <frappe@ubuntu.vm>
2022-04-10 10:22:02 +0000

    .

---
+++ mes/mes/doctype/indicator_uom/indicator_uom.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, BakrAldubai and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class IndicatorUOM(Document):
+	pass
